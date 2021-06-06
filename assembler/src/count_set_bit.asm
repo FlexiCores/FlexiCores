6c69a50f3f0377b04ad0d8fce41a5ac99d3009eb
lclee <lclee3@illinois.edu>
2021-06-06 20:17:28 +0800

    new simple programs for FlexiCore

---
+++ assembler/src/count_set_bit.asm
@@ -0,0 +1,39 @@
+; Count number of 1s.
+;    Inputs: R2 - only operand
+;   Outputs: R0 - number of 1s in the operand (in the range of 1 to 4)
+
+    nandi 0
+    addi 1
+    store r0        ; clear R0
+    load r0
+    addi 0          ; set operand (R2)
+    store r2        
+    load r0
+    addi 3
+    store r3
+
+SHIFT:
+    load r2
+    branch INCREMENT
+CONTINUE:
+    load r2
+    add r2
+    store r2
+    load r3
+    addi 0xf
+    store r3
+    branch END
+    branch SHIFT
+    xori 0xf
+    branch SHIFT
+    
+
+INCREMENT:
+    load r0
+    addi 1
+    store r0
+    branch CONTINUE
+    xori 0xf
+    branch CONTINUE
+
+END:
---
+++ assembler/src/multiply.asm
@@ -0,0 +1,47 @@
+; Multiply two numbers together.
+;    Inputs: R2 - first operand
+;            R3 - second operand
+;   Outputs: R0 - product of multiplication
+; Important: Overflow not accounted for.
+
+    nandi 0
+    addi 1
+    store r0        ; clear R0
+    load r0
+    addi 3          ; set first operand (R2)
+    store r2        
+    load r0 
+    addi 2          ; set second operand (R3)
+    store r3        
+    branch OPERAND
+    load r3
+    addi 0xf
+    store r3
+    branch END      ; check if second operand is 0
+
+MULTIPLY:
+    load r2
+    add r0
+    store r0
+    load r3
+    addi 0xf
+    store r3
+    branch END
+    xori 0xf
+    branch MULTIPLY
+
+OPERAND:
+    load r2
+    add r0
+    store r0
+    load r3
+    addi 0xf
+    store r3
+    branch OPERAND
+    load r3
+    addi 0xf
+    store r3
+    xori 0xf
+    branch MULTIPLY
+
+END:
