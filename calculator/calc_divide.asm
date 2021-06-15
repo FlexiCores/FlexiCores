66fb4c3a8eca91eff467b3258b4b5bad814291e4
lclee <lclee3@illinois.edu>
2021-06-15 12:34:40 +0800

    Division now supports negative numbers in 2's complement

--- calculator/calc_divide.asm
+++ calculator/calc_divide.asm
@@ -1,10 +1,11 @@
 ; Calculator Operation: divide one number from another.
 ; Return to linkage after opertion completed.
-;    Inputs: R2 - first operand (must be positive in 2's complement)
-;            R3 - second operand (must be positive in 2's complement)
-;   Outputs: R0 - quotient and remainder (displayed over 2 cycles, first R7 then R6)
+;    Inputs: R2 - first operand (in 2's complement)
+;            R3 - second operand (in 2's complement)
+;   Outputs: R0 - quotient and remainder (in 2's complement) (displayed over 2 cycles, first R7 then R6)
 ; Registers: R7 - temporary buffer for quotient
 ;            R6 - temporary buffer for remainder
+; Important: Even though the assembly file is 141 lines. The assembled file contains only 108 (< 128) instructions.
 
     load r5
     addi 3
@@ -12,26 +13,33 @@
     load r5
     store r0
     
+    load r2
+    branch NX
+    load r3
+    branch PN
+
     load r3                 ; Negate R3
     nandi 0xf
     addi 1
     store r3                ; Negate R3
 
-SUBTRACT:
+PP_SUBTRACT:
     load r2
     add r3
-    branch REMAINDER
+    branch PP_REMAINDER
     store r2
     load r7
     addi 1
     store r7
-    branch SUBTRACT
+    branch PP_SUBTRACT
     xori 0xf
-    branch SUBTRACT
+    branch PP_SUBTRACT
 
-REMAINDER:
+PP_REMAINDER:
     load r2
     store r6
+    branch END
+    xori 0xf
     branch END
 
 END:
@@ -50,5 +58,92 @@ END:
     store r0
     xori 0
     xori 0
-    xori 0
-    xori 0
+
+NX:
+    load r3
+    branch NN
+    xori 0xf
+    branch NP
+
+PN:
+    load r2
+    add r3
+    branch PN_REMAINDER
+    store r2
+    load r7
+    addi 1
+    store r7
+    branch PN
+    xori 0xf
+    branch PN
+
+PN_REMAINDER:
+    load r2
+    store r6
+    load r7
+    nandi 0xf
+    addi 1
+    store r7
+    branch END
+    xori 0xf
+    branch END
+
+NN:
+    load r2
+    nandi 0xf
+    addi 1
+    store r2
+NN_SUBTRACT:
+    load r2
+    add r3
+    branch NN_REMAINDER
+    store r2
+    load r7
+    addi 1
+    store r7
+    branch NN_SUBTRACT
+    xori 0xf
+    branch NN_SUBTRACT
+
+NN_REMAINDER:
+    load r2
+    nandi 0xf
+    addi 1
+    store r6
+    branch END
+    xori 0xf
+    branch END
+
+NP:
+    load r2
+    nandi 0xf
+    addi 1
+    store r2
+    load r3       
+    nandi 0xf
+    addi 1
+    store r3                
+NP_SUBTRACT:
+    load r2
+    add r3
+    branch NP_REMAINDER
+    store r2
+    load r7
+    addi 1
+    store r7
+    branch NP_SUBTRACT
+    xori 0xf
+    branch NP_SUBTRACT
+
+NP_REMAINDER:
+    load r2
+    nandi 0xf
+    addi 1
+    store r6
+    load r7
+    nandi 0xf
+    addi 1
+    store r7
+    branch END
+    xori 0xf
+    branch END
