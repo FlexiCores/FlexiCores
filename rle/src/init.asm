e04bdcfcdd9f7c5139ab84e8d0e8fb3c0283c551
lclee <lclee3@illinois.edu>
2021-10-19 00:13:27 -0500

    RLE 4 bit

---
+++ rle/src/init.asm
@@ -0,0 +1,78 @@
+; Run-Length Encoder
+;
+;      Inputs: a time series of input values, denoted by x(t)                            {0, 1}   
+;     Outputs: a sequence of numbers encoding the number of 0s and 1s in the input       [0, 15]
+;           
+;              -> the first output indicates whether the input stream starts with 0 or 1
+;              -> each additional output indicates the number of consecutive 0s and 1s
+;
+;     Example: RLE[0000 11111 000] = 0, 4, 5, 3
+;   IMPORTANT: the RLE will run indefinitely until the end of input stream is reached.
+;              the input stream terminates in 0xF.
+;     
+;
+; Performance: 
+;   Registers: R2    - prev input
+;              R3    - curr input
+;              R4    -  
+;              R5    - 0
+;              R6    - 
+;              R7    - counter  
+;
+; -> Inputs read from IPORT, outputs displayed to OPORT
+
+    nandi 0
+    addi 1
+    store r5                ; clear R5   
+
+    load r5
+    addi 3
+    store r0                ; reset FSM
+
+    load r5
+    store r7                ; clear output
+
+    load r5
+    addi 2
+    store r0
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 2
+    store r0                
+    load r1 
+    store r2                ; IPORT input
+    load r5
+    addi 3
+    store r0                ; reset FSM
+
+;   Display the first output to indicate whether the bit stream start with 0 or 1
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 0
+    store r0
+    load r5
+    addi 2
+    store r0    
+    load r2
+    store r0                ; output displayed
+    load r5
+    addi 3
+    store r0                ; reset FSM
+
+    load r7
+    addi 1
+    store r7                ; increment counter
+
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 2
+    store r0
+    load r5
+    addi 0
+    store r0                ; LOOP
---
+++ rle/src/loop.asm
@@ -0,0 +1,115 @@
+; See init.asm for program description
+
+    load r5
+    addi 3
+    store r0                ; reset FSM
+
+    load r5
+    addi 2
+    store r0
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 2
+    store r0                
+    load r1 
+    store r3                ; IPORT input
+    load r5
+    addi 3
+    store r0                ; reset FSM
+
+    load r3
+    branch END  
+
+    load r3
+    add r3
+    store r4
+    add r4
+    store r4
+    add r4
+    store r4
+
+    load r2
+    add r2
+    store r2
+    add r2
+    store r2
+    add r2
+    store r2
+
+    load r2
+    xor r4
+    branch INPUT_CHANGED
+    
+    load r3
+    store r2
+
+    load r7
+    addi 1
+    store r7
+    branch LOOP
+    xori 0xf
+    branch LOOP
+
+INPUT_CHANGED:
+
+    load r3
+    store r2
+
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 0
+    store r0
+    load r5
+    addi 2
+    store r0    
+    load r7
+    store r0                ; output displayed
+    load r5
+    addi 3
+    store r0                ; reset FSM
+
+
+    load r5
+    addi 1
+    store r7
+
+LOOP:
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 2
+    store r0
+    load r5
+    addi 0
+    store r0                ; LOOP
+
+END:
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 0
+    store r0
+    load r5
+    addi 2
+    store r0    
+    load r7
+    store r0                ; output displayed
+    load r5
+    addi 3
+    store r0                ; reset FSM
+    
+    load r5
+    addi 1
+    store r0
+    load r5
+    addi 2
+    store r0
+    load r5
+    addi 1
+    store r0 
