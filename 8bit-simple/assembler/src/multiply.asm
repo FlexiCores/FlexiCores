3663f79866ce1482eed20b74cc9cc2be49d02b11
lclee <lclee3@illinois.edu>
2021-11-09 16:48:15 -0600

    testing tb

--- 8bit-simple/assembler/src/multiply.asm
+++ 8bit-simple/assembler/src/multiply.asm
@@ -8,10 +8,11 @@
     addi 1
     store r0        ; clear R0
     load r0
-    addi 0xf          ; set first operand (R2)
+    addi 0x7          ; set first operand (R2)
+    addi 0x7          ; set first operand (R2)
     store r2        
     load r0 
-    addi 0xf          ; set second operand (R3)
+    addi 0x5          ; set second operand (R3)
     store r3        
     branch OPERAND
     load r3
